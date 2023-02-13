37fd575166a5fc96db5cf6ecc4a2e7a8a76d082a
Sara Wasim <sara.wasim@elasticpath.com>
2023-02-13 01:35:33 -0800

    adjusted margin

--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -66,7 +66,7 @@ function Carousel({ images }) {
                 maxWidth="500px"
                 p={4}
                 textAlign="left"
-                marginRight="30px"
+                marginRight={{ base: "0", sm: "0", md: "30px", lg: "30px" }}
                 backgroundColor="rgba(239, 239, 240,0.6)"
                 borderRadius={10}
                 display={image.display}
